3c9c566a150efac1260bdddb90ea981f86ee3cbf
Michaeliaaa <michaelia0816@me.com>
2021-01-09 00:17:38 +0800

    Update database and add pleasant visual elements

--- app/src/main/java/com/example/opencvproject/DatabaseHelper.java
+++ app/src/main/java/com/example/opencvproject/DatabaseHelper.java
@@ -7,28 +7,28 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.provider.ContactsContract;
 
+import java.sql.Time;
 import java.util.Date;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
 
-    public static final String DATABASE_NAME = "Stats.db";
+    public static final String DATABASE_NAME = "focuslab.db";
     public static final String TABLE_NAME = "track_table";
 
 
-    // COLUMNS
-    public static final String COL0 = "Date";
-    public static final String COL1 = "TimeFocused";
+//    // COLUMNS
+//    public static final String COL0 = "Date";
+//    public static final String COL1 = "TimeFocused";
 
 
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, 1);
-
     }
     @Override
     public void onCreate(SQLiteDatabase db) {
 //      DATE: A date. Format: YYYY-MM-DD. The supported range is from '1000-01-01' to '9999-12-31'
         //store date as LONG
-        String createTable = "CREATE TABLE " + TABLE_NAME + "(Date INTEGER PRIMARY KEY, TimeFocused FLOAT(23))";
+        String createTable = "CREATE TABLE " + TABLE_NAME + "(date TEXT PRIMARY KEY, TimeFocused TEXT)";
         db.execSQL(createTable);
     }
 
@@ -38,36 +38,59 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         onCreate(db);
     }
 
+    public Boolean insertUserData(String date, String TimeFocused) {
+        SQLiteDatabase DB = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("date", date); //(column name, value)
+        contentValues.put("TimeFocused", TimeFocused);
+        long result = DB.insert(TABLE_NAME, null, contentValues);
 
+        if (result == -1) {
+            return false;
+        } else {
+            return true;
+        }
+    }
 
-//    public boolean addData(String item) {
-//        SQLiteDatabase db = this.getWritableDatabase();
-//        ContentValues contentValues = new ContentValues();
-//        contentValues.put(COL1, item);
-//        long result = db.insert(TABLE_NAME, null, contentValues);
-//
-//        //if data inserted wrongly return -1
-//        if (result == -1) {
-//            return false;
-//        }
-//        else {
-//            return true;
-//        }
-//    }
+    public Boolean updateUserData(String date, String TimeFocused) {
+        SQLiteDatabase DB = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("date", date); //(column name, value)
+        contentValues.put("TimeFocused", TimeFocused);
+        Cursor cursor = DB.rawQuery("SELECT * FROM " + TABLE_NAME + " WHERE date = ?", new String[]{date});
+        if (cursor.getCount()>0) {
+            long result = DB.update(TABLE_NAME, contentValues, "date=?", new String[]{date});
+            if (result == -1) {
+                return false;
+            } else {
+                return true;
+            }
+        } else {
+            return false;
+        }
+    }
 
+    public Boolean deleteUserData(String date) {
+        SQLiteDatabase DB = this.getWritableDatabase();
 
-//    public Cursor getData() {
-//        SQLiteDatabase db = this.getWritableDatabase();
-//        String query = "SELECT * FROM " + TABLE_NAME;
-//        Cursor data = db.rawQuery(query, null);
-//        return data;
-//
-//        //to obtain data into an arraylist:
-//        // Cursor data = mDatabaseHelper.getData();
-//        // ArrayList<String> listData = new ArrayList<>();
-//        // while (data.moveToNext()) {
-//        //    listData.add(data.getString(1));
-//        // }
-//
-//    }
+        Cursor cursor = DB.rawQuery("SELECT * FROM " + TABLE_NAME + " WHERE date = ?", new String[]{date});
+        if (cursor.getCount()>0) {
+            long result = DB.delete(TABLE_NAME, "date=?", new String[]{date});
+            if (result == -1) {
+                return false;
+            } else {
+                return true;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    public Cursor getData() {
+        SQLiteDatabase DB = this.getWritableDatabase();
+
+        Cursor cursor = DB.rawQuery("SELECT * FROM " + TABLE_NAME, null);
+        return cursor;
+    }
+
 }
